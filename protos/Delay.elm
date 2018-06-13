cddeca544f3aca8176626e2c9453cd77c98e2b5b
Ilmari Koskinen <ilmari.koskinen@houston-inc.com>
2018-06-13 10:41:52 +0300

    Add delayed cmd msg example

--- protos/Delay.elm
+++ protos/Delay.elm
@@ -3,39 +3,54 @@ module Delay exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Time
+import Time exposing (Time, second)
 import Task
 import Process
 
-delay : Time.Time -> msg -> msg
+type alias Model =
+  Int
+
+main : Program Never Model Msg
+main =
+    program
+        { init = (0, Cmd.none)
+        , view = view
+        , update = update
+        , subscriptions = (always Sub.none)
+        }
+
+delay : Time.Time -> msg -> Cmd msg
 delay time msg =
   Process.sleep time
   |> Task.perform (\_ -> msg)
 
-main =
-  beginnerProgram { model = 0, view = view, update = update }
-
+view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
     , button [ onClick Reset ] [ text "Reset" ]
-    , button [ onClick (delay (Time.second * 1) <| Increment) ] [ text "Delay Increment" ]
+    , button [ onClick DelayedIncrement ] [ text "Delay Increment" ]
     ]
 
 type Msg =
   Increment
   | Decrement
   | Reset
+  | DelayedIncrement
 
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      (model + 1) ! []
 
     Decrement ->
-      model - 1
+      (model - 1) ! []
 
     Reset ->
-      0
+      (0) ! []
+
+    DelayedIncrement ->
+      model ! [delay (second * 1) <|  Increment]
